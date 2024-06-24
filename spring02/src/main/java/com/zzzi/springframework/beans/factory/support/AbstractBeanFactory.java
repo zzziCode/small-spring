5b33914aac0cb728265e1a997b2af8d1378625ad
zzziCode <1980136696@qq.com>
2024-06-24 14:01:54 +0800

    :smile: update come files!

--- spring02/src/main/java/com/zzzi/springframework/beans/factory/support/AbstractBeanFactory.java
+++ spring02/src/main/java/com/zzzi/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -5,21 +5,23 @@ import com.zzzi.springframework.beans.factory.config.BeansException;
 import com.zzzi.springframework.beans.factory.config.BeanFactory;
 import com.zzzi.springframework.beans.factory.config.BeanDefinition;
 
-/**@author zzzi
+/**
+ * @author zzzi
  * @date 2023/10/30 12:48
  * 继承了一个类,实现了一个接口
  * DefaultSingletonBeanRegistry类中的方法已经全部实现了,在这里继承的目的只是为了使用父类的方法
  * BeanFactory接口中的方法还没有实现,在这里的目的就是为了实现其中的方法
- *
+ * <p>
  * 同时提供了两个抽象方法供下面的类实现
- *
+ * <p>
  * 这个类继承的到的方法有:
  * 1.getBean:尝试获取一个单例模式的bean对象,没有就创建
  * 2.getSingleton:尝试获取一个单例模式的bean对象并返回
  * 3.addSingleton:将新创建的bean对象保存到容器中
  */
 public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
-    /**@author zzzi
+    /**
+     * @author zzzi
      * @date 2023/10/30 12:49
      * 将这个唯一的没有实现的方法在这里实现
      * 尝试获取一个单例模式的bean对象
@@ -29,7 +31,7 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
     public Object getBean(String name) throws BeansException {
         //尝试直接获取，这里保证了bean的单例
         Object singleton = getSingleton(name);
-        if(singleton!=null){
+        if (singleton != null) {
             return singleton;
         }
 
@@ -39,16 +41,18 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
         BeanDefinition beanDefinition = getBeanDefinition(name);
 
         //2.根据类信息创建bean的实例化对象,并将其保存到容器中
-        return createBean(name,beanDefinition);
+        return createBean(name, beanDefinition);
     }
 
-    /**@author zzzi
+    /**
+     * @author zzzi
      * @date 2023/10/29 21:43
      * 在类DefaultListableBeanFactory中实现
      */
     protected abstract BeanDefinition getBeanDefinition(String beanName) throws BeansException;
 
-    /**@author zzzi
+    /**
+     * @author zzzi
      * @date 2023/10/29 21:44
      * 在类AbstractAutowireCapableBeanFactory中实现
      */
